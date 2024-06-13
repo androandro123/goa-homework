603de52bbee19035d2c32dd4207b6f3a203a8d79
androandro123 <khoravaandro@gmail.com>
2024-06-13 21:08:28 +0400

    second commit

---
+++ level 10/homework.py
@@ -0,0 +1,2 @@
+age=int((input("hey,what is your age?")))
+print("you are going to be " + age + 36 + " after 36 years")
---
+++ level 10/homework2.py
@@ -0,0 +1,4 @@
+name=str(input("my name:"))
+last_name=str(input("my last_name:"))
+age=str(input("my age:"))
+print("hi,my name is "+name+",my last name is "+last_name+",my age is "+age)
