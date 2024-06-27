585076454dd0a2bfc3b93d8e11eca5fbb674e707
androandro123 <khoravaandro@gmail.com>
2024-06-27 20:59:02 +0400

    second commit

---
+++ level 14/classwork 2.py
@@ -0,0 +1,10 @@
+# and
+print(10 == 20 and 10 > 20)
+print(9 == 25 and 23 > 12)
+print(15 <= 21 and 40 > 100)
+print(11 == 11 and 40 <= 100)
+# or
+print(10 == 21 or 10 > 20)
+print(9 == 25 or 23 <= 12)
+print(15 <= 21 or 40 > 100)
+print(11 == 11 or 40 <= 100)
---
+++ level 14/classwork.py
@@ -0,0 +1,23 @@
+print(10 == 20)
+print(9 == 25)
+print(15 == 21)
+
+print(10 != 20)
+print(15 != 21)
+print(9 != 25)
+
+print(18 < 20)
+print(10 < 30)
+print(15 < 20)
+
+print(10 > 20)
+print(6 > 20)
+print(10 > 10)
+
+print(10 <= 20)
+print(2 <= 17)
+print(16 <= 29)
+
+print(10 >= 40)
+print(12 >= 23)
+print(20 >= 20)
