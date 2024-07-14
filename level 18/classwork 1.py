7a5d43d7dafce24d869ff7f690c4672f92bdf43b
androandro123 <khoravaandro@gmail.com>
2024-07-14 13:26:26 +0400

    second commit

---
+++ level 18/classwork 1.py
@@ -0,0 +1,6 @@
+for i in range(30): #classwork1
+    print('hello world')
+for number in range(10):#classworrk2
+    print(number)
+for num in range(10,20,2):
+    print(num)
---
+++ level 18/homework 1.py
@@ -0,0 +1,35 @@
+for i in range(10): #homework 1
+    print(i)
+    print('hello world')
+for num in range(10): #homework 2
+    print(num + num)
+number = 0
+while number != 100: #homework 3
+    print(number)
+    number = number + 1
+for i in range(101,1000):
+    print(i)
+a = 100 #homework 4
+while a != 0:
+    print(a)
+    a = a - 1
+num = 0 #homework 5
+while num != 10:
+    print(num)
+    print('hello world')
+    num = num + 1
+num = 0 #homework 6
+while num != 10:
+    print(num + num)
+    num = num + 1
+num = 0 #homework 7
+while num != 100:
+    print(num)
+    num = num + 1
+for num in range(101,1000):
+    print(num)
+num = 100 #homework 8
+while num != 0:
+    print(num)
+    num = num - 1
+
