b8f435b679ebd12dc67dc3155267bbe755217ad9
androandro123 <khoravaandro@gmail.com>
2024-07-28 13:08:34 +0400

    seccond commit

---
+++ level 22/classwork 1.py
@@ -0,0 +1,25 @@
+#                   -3         -2        -1
+shopping_list = ["bread", "chiken", "ketchup"]
+#                   0         1          2
+print(shopping_list[1])
+print(shopping_list[-1])
+#           -3           -2           -1
+tools = ["hammer", "screwdriver", "nails"]
+#           0            1            2
+print(tools[2])
+print(tools[-2])
+#            -3              -2        -1
+games = ["genshin impact", "GTA", "brawl stars"]
+#            0                1          2
+print(games[2])
+print(games[-1])
+#                -4        -3      -2         -1
+directions = ["right", "left", "forward" , "back"]
+#                0         1        2         3
+print(directions[3])
+print(directions[-2])
+#              -2          -1
+sites = ["wikipedia", "github"]
+#             0           1
+print(sites[1])
+print(sites[-2])
---
+++ level 22/classwork 2.py
@@ -0,0 +1,3 @@
+sports = ["karate", "football" , "basketball" , "boxing", "swimming"]
+print(sports[0])
+print(sports[-1])
